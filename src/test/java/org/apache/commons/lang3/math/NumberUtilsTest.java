73b850dea73dbfadb2b48762d03ca7e0d4c872d0
Emil Hultcrantz <emilhtz@gmail.com>
2024-02-22 01:36:27 +0100

    Add comments explaining new tests

--- src/test/java/org/apache/commons/lang3/math/NumberUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/math/NumberUtilsTest.java
@@ -781,9 +781,13 @@ public class NumberUtilsTest extends AbstractLangTest {
         compareIsCreatableWithCreateNumber("11 11", false);
         compareIsCreatableWithCreateNumber(" 1111", false);
         compareIsCreatableWithCreateNumber("1111 ", false);
+        // Test that 0x retruns false
         compareIsCreatableWithCreateNumber("0x", false);
+        // Test that two E's retruns false
         compareIsCreatableWithCreateNumber("12EE1", false);
+        // Test that '.' as last character with a previous Ereturns false
         compareIsCreatableWithCreateNumber("12E.", false);
+        // Test that '.' as last character with a previous '.' returns false
         compareIsCreatableWithCreateNumber("12.1.", false);
 
         compareIsCreatableWithCreateNumber("2.", true); // LANG-521
